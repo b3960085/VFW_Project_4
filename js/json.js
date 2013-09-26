445bd191a3d9c9b40d25dabe2eaa5830790ba838
Jesse Read <jread@invokedcode.net>
2013-09-26 09:28:11 -0400

    Added more sample data.

--- js/json.js
+++ js/json.js
@@ -2,11 +2,32 @@ var sampleData = {
 	"dpram" : {
 		"artistName" : ["Album Artist: ", "Daft Punk"],
 		"albumName" : ["Album Name: ", "Random Access Memories"],
-		"releaseDate" : ["Release Date: ", "2013-07-01"],
+		"releaseDate" : ["Release Date: ", "2013-05-17"],
 		"releaseType" : ["Release Type: ", "Album"],
 		"releaseArtist" : ["Release Artists: ", "Single Artist"],
 		"songCount" : ["Number of songs: ", "13"],
-		"opinion" : ["Opinion: ", "Great!"],
+		"opinion" : ["Opinion: ", "Play on repeat. Always."],
 		"favorite" : ["Favorite: ", "Yes"],
+	},
+	"dsotm" : {
+		"artistName" : ["Album Artist: ", "Pink Floyd"],
+		"albumName" : ["Album Name: ", "Dark Side of the Moon"],
+		"releaseDate" : ["Release Date: ", "1973-03-01"],
+		"releaseType" : ["Release Type: ", "Album"],
+		"releaseArtist" : ["Release Artists: ", "Single Artist"],
+		"songCount" : ["Number of songs: ", "10"],
+		"opinion" : ["Opinion: ", "Classic."],
+		"favorite" : ["Favorite: ", "Yes"],		
+	}, 
+	"ob" : {
+		"artistName" : ["Album Artist: ", "Various Artists"],
+		"albumName" : ["Album Name: ", "The Orange Box"],
+		"releaseDate" : ["Release Date: ", "2007-12-21"],
+		"releaseType" : ["Release Type: ", "Compilation"],
+		"releaseArtist" : ["Release Artists: ", "Various Artists"],
+		"songCount" : ["Number of songs: ", "19"],
+		"opinion" : ["Opinion: ", "An interesting mix."],
+		"favorite" : ["Favorite: ", "No"],	
 	}
+	
 }
