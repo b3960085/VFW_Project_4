8401e9a565c507460d50baf0773ccd09ef10c808
Jesse Read <jread@invokedcode.net>
2013-09-26 08:15:51 -0400

    First sample object.

---
+++ js/json.js
@@ -0,0 +1,12 @@
+var sampleData = {
+	"dpram" : {
+		"artistName" : ["Album Artist: ", "Daft Punk"],
+		"albumName" : ["Album Name: ", "Random Access Memories"],
+		"releaseDate" : ["Release Date: ", "2013-07-01"],
+		"releaseType" : ["Release Type: ", "Album"],
+		"releaseArtist" : ["Release Artists: ", "Single Artist"],
+		"songCount" : ["Number of songs: ", "13"],
+		"opinion" : ["Opinion: ", "Great!"],
+		"favorite" : ["Favorite: ", "Yes"],
+	}
+}
